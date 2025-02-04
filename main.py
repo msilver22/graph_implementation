c2b9c20fd42f9149d1ccc0d5c3d8ef7be4008d2a
msilver22 <silvestri.m@di.uniroma1.it>
2025-02-04 14:16:03 +0100

    Update functions' time

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ def measure_time(graph_func, *args):
     start_time = time.time()
     graph_func(*args)
     end_time = time.time()
-    return round(end_time - start_time, 5)
+    return round(end_time - start_time, 8)
 
 
 def main():
 
-    #datasets = ["kar","words"]
     datasets = ["kar","words", "vote","pow","fb-75"]
-
     raws = []
 
     for dataset in datasets:
@@ -35,6 +33,60 @@ def main():
 
     headers = ["Network", "Size", "File Type", "Nx Time(s)", "Ig Time(s)"]
     print(create_markdown_table(headers, raws, "./outputs/import_time_table.md"))
+
+    functions = [ "Get Edge List", "Get Node List", "Get Neighbourhood", "Has Edge","Get Degree", "Get Betweenness Centrality", "Add Edge", "Remove Edge"]
+    raws = []
+
+    
+    for dataset in datasets:
+        print(dataset)
+        for func_name in functions:
+            print(func_name)
+            G1 = nx_import_graph(f"./datasets/data/{dataset}.txt")
+            G2 = ig_import_graph(f"./datasets/data/{dataset}.txt")
+            if G1.number_of_nodes() + G1.number_of_edges() == G2.vcount() + G2.ecount():
+                if func_name == "Get Edge List":
+                    nx_time=measure_time(nx.edges, G1)
+                    ig_time=measure_time(G2.get_edgelist)
+                elif func_name == "Get Node List":
+                    nx_time=measure_time(nx.nodes, G1)
+                    ig_time=measure_time(G2.vs)
+                elif func_name == "Get Neighbourhood":
+                    nx_time=measure_time(G1.neighbors, 1)
+                    ig_time=measure_time(G2.neighbors, 0)
+                elif func_name == "Has Edge":
+                    nx_time=measure_time(G1.has_edge, 1, 2)
+                    ig_time=measure_time(G2.are_adjacent, 0, 1)
+                elif func_name == "Get Degree":
+                    nx_time=measure_time(G1.degree, 1)
+                    ig_time=measure_time(G2.degree, 0)
+                elif func_name == "Get Betweenness Centrality":
+                    nx_time=measure_time(nx.betweenness_centrality,G1)
+                    ig_time=measure_time(G2.betweenness)
+                elif func_name == "Add Edge":
+                    if not G2.are_adjacent(0, 1):
+                        nx_time=measure_time(G1.add_edge, 1, 2)
+                        ig_time=measure_time(G2.add_edge, 0, 1)
+                    else:
+                        neighbors = set(G2.neighbors(0))
+                        non_neighbors = set(range(G2.vcount())) - neighbors
+                        v = non_neighbors.pop()
+                        nx_time=measure_time(G1.add_edge, 1, v+1)
+                        ig_time=measure_time(G2.add_edge, 0, v)   
+
+                elif func_name == "Remove Edge":
+                    if G2.are_connected(0, 1):
+                        nx_time=measure_time(G1.add_edge, 1, 2)
+                        ig_time=measure_time(G2.add_edge, 0, 1)
+                    else:
+                        neighbors = set(G2.neighbors(0))
+                        v = neighbors.pop()
+                        nx_time=measure_time(G1.remove_edge, 1, v+1)
+                        ig_time=measure_time(G2.delete_edges, 0, v)  
+                raws.append([dataset, G1.number_of_nodes() + G1.number_of_edges(), func_name, nx_time, ig_time])
+
+    headers = ["Network", "Size", "Function", "Nx Time(s)", "Ig Time(s)"]
+    print(create_markdown_table(headers, raws, "./outputs/functions_time_table.md"))
 
 if __name__ == "__main__":
     main()
---
+++ outputs/functions_time_table.md
@@ -0,0 +1,42 @@
+| Network | Size | Function | Nx Time(s) | Ig Time(s) |
+| --- | --- | --- | --- | --- |
+| kar | 112 | Get Edge List | 1.001e-05 | 0.00069809 |
+| kar | 112 | Get Node List | 7.87e-06 | 1.19e-06 |
+| kar | 112 | Get Neighbourhood | 1.19e-06 | 9.5e-07 |
+| kar | 112 | Has Edge | 7.2e-07 | 9.5e-07 |
+| kar | 112 | Get Degree | 2.15e-06 | 0.0 |
+| kar | 112 | Get Betweenness Centrality | 0.00112987 | 0.00031281 |
+| kar | 113 | Add Edge | 9.5e-07 | 1.001e-05 |
+| kar | 112 | Remove Edge | 9.5e-07 | 5.96e-06 |
+| words | 537 | Get Edge List | 2.15e-06 | 2.599e-05 |
+| words | 537 | Get Node List | 2.15e-06 | 1.67e-06 |
+| words | 537 | Get Neighbourhood | 9.5e-07 | 0.0 |
+| words | 537 | Has Edge | 9.5e-07 | 7.2e-07 |
+| words | 537 | Get Degree | 9.5e-07 | 1.19e-06 |
+| words | 537 | Get Betweenness Centrality | 0.01190615 | 0.00059295 |
+| words | 538 | Add Edge | 1.19e-06 | 1.097e-05 |
+| words | 537 | Remove Edge | 9.5e-07 | 6.91e-06 |
+| vote | 3803 | Get Edge List | 4.05e-06 | 0.00014091 |
+| vote | 3803 | Get Node List | 4.05e-06 | 1.91e-06 |
+| vote | 3803 | Get Neighbourhood | 9.5e-07 | 9.5e-07 |
+| vote | 3803 | Has Edge | 9.5e-07 | 0.0 |
+| vote | 3803 | Get Degree | 1.19e-06 | 9.5e-07 |
+| vote | 3803 | Get Betweenness Centrality | 0.84270692 | 0.0303371 |
+| vote | 3804 | Add Edge | 1.19e-06 | 6.771e-05 |
+| vote | 3802 | Remove Edge | 8.11e-06 | 9.799e-05 |
+| pow | 11535 | Get Edge List | 5.01e-06 | 0.00031519 |
+| pow | 11535 | Get Node List | 4.05e-06 | 2.15e-06 |
+| pow | 11535 | Get Neighbourhood | 1.91e-06 | 2.15e-06 |
+| pow | 11535 | Has Edge | 0.0 | 0.0 |
+| pow | 11535 | Get Degree | 2.15e-06 | 1.67e-06 |
+| pow | 11535 | Get Betweenness Centrality | 22.33310103 | 0.60359192 |
+| pow | 11536 | Add Edge | 3.1e-06 | 0.00028896 |
+| pow | 11534 | Remove Edge | 1.91e-06 | 0.00028896 |
+| fb-75 | 224048 | Get Edge List | 5.96e-06 | 0.01349401 |
+| fb-75 | 224048 | Get Node List | 4.05e-06 | 3.81e-06 |
+| fb-75 | 224048 | Get Neighbourhood | 9.5e-07 | 4.05e-06 |
+| fb-75 | 224048 | Has Edge | 9.5e-07 | 9.5e-07 |
+| fb-75 | 224048 | Get Degree | 2.15e-06 | 2.15e-06 |
+| fb-75 | 224048 | Get Betweenness Centrality | 221.96496201 | 7.73602891 |
+| fb-75 | 224049 | Add Edge | 2.15e-06 | 0.00392127 |
+| fb-75 | 224047 | Remove Edge | 2.15e-06 | 0.00439501 |
